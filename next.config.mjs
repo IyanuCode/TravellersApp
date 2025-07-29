9aa810a9dea1cfb16921974b2f103353969fd744
Adeusi Iyanu Emmanuel <adeusiiyanu7@gmail.com>
2025-07-29 05:55:07 +0100

    project live

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: '/TravellersApp', // ✅ matches your repo name
+  assetPrefix: '/TravellersApp/', // ✅ same here
+};
 
 export default nextConfig;
